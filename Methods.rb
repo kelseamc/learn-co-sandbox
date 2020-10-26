1ba9cd256a1445d3127b02936abd52b687b9a389
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-26 14:35:12 +0000

    Automatically backed up by Learn

---
+++ Methods.rb
@@ -0,0 +1,15 @@
+def greeting #method signature
+  puts "Hello World"
+end
+
+greeting #Call the  Method and exicute the body of code
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+say_greeting_five_times
---
+++ build_case.rb
@@ -0,0 +1,9 @@
+#Create a value 
+case greeting
+#Create conditions
+When "unfriendsly_greeting"
+#Add the code
+  puts "What do you want!?"
+When "friendly_greeting"
+  puts "Hi! How are you?"
+end
